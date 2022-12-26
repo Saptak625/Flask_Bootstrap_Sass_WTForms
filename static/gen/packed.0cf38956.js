a2277eb402f33448a50b2c9fce03fd04b23fcc87
SaptakDnth <66028584+Saptak625@users.noreply.github.com>
2022-12-26 11:13:50 -0500

    Remove Gen Files and Create app.py

--- static/gen/packed.0cf38956.js
+++
@@ -1,9 +0,0 @@
-function loading(){alert("Loading... Please wait.")
-const data=document.getElementById("data")
-if(data!==null){data.style.display='none';}
-const spinner=document.getElementById("loadSpinner");spinner.parentElement.style.display='block';spinner.style.display='block';}
-(()=>{'use strict'
-const forms=document.querySelectorAll('.needs-validation')
-Array.from(forms).forEach(form=>{form.addEventListener('submit',event=>{if(!form.checkValidity()){event.preventDefault()
-event.stopPropagation()}else{loading();}
-form.classList.add('was-validated')},false)})})()
